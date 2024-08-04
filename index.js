35a50076c38a4ada09c05f636c9c6754782a0ce4
Gursimran27 <singhgursimran165@gmail.com>
2024-08-04 14:36:07 +0530

    bug fixing

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
-const isDev = app.settings.env === 'development'
+// const isDev = app.settings.env === "development";
 // const URL = isDev ? 'http://localhost:3000' : 'https://sketchbook-sigma.vercel.app'
-app.use(cors({origin: "*"}))
+app.use(cors({ origin: "*" }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "*", // or specific domain if known
-  },});
+  },
+});
 
-  io.on('connection', (socket) => {
-    console.log("server is connected")
+io.on("connection", (socket) => {
+  console.log("server is connected");
 
-    socket.on('join-room', (roomId, userId) => {//userId is peerID i.e SDP
-        console.log(`a new user ${userId} joined room ${roomId}`)
-        socket.join(roomId)
-        socket.broadcast.to(roomId).emit('user-connected', userId)//broadcast to all users except the currrent one
-    })
+  socket.on("join-room", (roomId, userId) => {
+    //userId is peerID i.e SDP
+    console.log(`a new user ${userId} joined room ${roomId}`);
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId); //broadcast to all users except the currrent one
+  });
 
-    socket.on('user-toggle-audio', (userId, roomId) => {
-        socket.join(roomId)
-        socket.broadcast.to(roomId).emit('user-toggle-audio', userId)
-    })
+  socket.on("user-toggle-audio", (userId, roomId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-toggle-audio", userId);
+  });
 
-    socket.on('user-toggle-video', (userId, roomId) => {
-        socket.join(roomId)
-        socket.broadcast.to(roomId).emit('user-toggle-video', userId)
-    })
+  socket.on("user-toggle-video", (userId, roomId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-toggle-video", userId);
+  });
 
-    socket.on('user-leave', (userId, roomId) => {
-        socket.join(roomId)
-        socket.broadcast.to(roomId).emit('user-leave', userId)
-    })
-})
+  socket.on("user-leave", (userId, roomId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-leave", userId);
+  });
+});
 
 httpServer.listen(5000);
