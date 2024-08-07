9683caa9e9585ef74d5877f6107ec1b2a21da7f3
Gursimran27 <singhgursimran165@gmail.com>
2024-08-07 15:30:11 +0530

    canvas added

--- index.js
+++ index.js
@@ -34,9 +34,47 @@ io.on("connection", (socket) => {
     socket.broadcast.to(roomId).emit("user-toggle-video", userId);
   });
 
+  socket.on("handleTooglePaint", (roomId, isOpened) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("handleTooglePaint", isOpened);
+  });
+
+  socket.on("beginPath", (arg) => {
+    socket.join(arg.roomId);
+    socket.broadcast.to(arg.roomId).emit("beginPath", arg);
+  });
+
+  socket.on("drawLine", (arg) => {
+    socket.join(arg.roomId);
+    socket.broadcast.to(arg.roomId).emit("drawLine", arg);
+  });
+
+  socket.on("changeConfig", (arg) => {
+    socket.join(arg.roomId);
+    socket.broadcast.to(arg.roomId).emit("changeConfig", arg);
+  });
+
+  socket.on("updateMenuItem", (itemName, roomId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("updateMenuItem", itemName);
+  });
+
+  socket.on("updateMenuAction", (itemName, roomId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("updateMenuAction", itemName);
+  });
+
+  socket.on("mouseUp", (roomId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("captureCanvasOnMouseUp");
+  });
+
   socket.on("user-leave", (userId, roomId) => {
     socket.join(roomId);
     socket.broadcast.to(roomId).emit("user-leave", userId);
+    socket.leave(roomId);
+    // socket.disconnect(0);
+    console.log('diconnecting from socket server and roomId ',roomId)
   });
 });
 
